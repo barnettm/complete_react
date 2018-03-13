4381c451faac7137e9a4db4cfcfa4cb2596cad9b
barnettm <barnett717@gmail.com>
2018-03-13 16:43:23 -0700

    jsx playground

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,68 @@
 
 console.log('app.js is running');
 
+var app = {
+    title: 'Indecision',
+    subtitle: 'Just another awesome app'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var user = {
+    name: "Matt",
+    age: 35,
+    location: 'Orange County'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoot = document.querySelector('#app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 console.log('app.js is running')
 
-var template = <h1>Indecision</h1>
+const app = {
+    title: 'Indecision',
+    subtitle: 'Just another awesome app'
+}
+
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: "Matt",
+    age: 35,
+    location: 'Orange County'
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+
+
+
 var appRoot = document.querySelector('#app')
 
 ReactDOM.render(template, appRoot)
