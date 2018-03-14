3a1029ea7085a3f18fea7af95bc1fbf5618db49d
barnettm <barnett717@gmail.com>
2018-03-14 14:09:55 -0700

    arrow function playground

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,53 @@
 'use strict';
 
-var fullName = "Matt Barnett";
+// const fullName = "Matt Barnett"
 
-var getFirstName = function getFirstName(name) {
-    return name.split(' ')[0];
+// const getFirstName = (name) => {
+//     return name.split(' ')[0];
+// }
+
+// console.log(getFirstName(fullName))
+
+
+// const getFirstNameTwo = (name) => name.split(' ')[1];
+
+// console.log(getFirstNameTwo(fullName))
+
+// arguments object - no longer bound with arrow functions
+// const add =  (a,b) => {
+//     console.log(arguments)
+//     return a+b;
+// }
+
+// console.log(add(55,1))
+
+var user = {
+    name: 'Matt',
+    cities: ['Ontario', 'Yorba Linda', 'Alta Loma'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        var cityMessages = this.cities.map(function (city) {
+            return _this.name + ' has lived in ' + city;
+        });
+        // this.cities.forEach((city) => {
+        //     console.log(this.name + ' has lived in ' + city)
+        // })
+    }
 };
 
-console.log(getFirstName(fullName));
+user.printPlacesLived(user.cities);
 
-var getFirstNameTwo = function getFirstNameTwo(name) {
-    return name.split(' ')[1];
+var multiplier = {
+    numbers: [1, 2, 3, 4],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
 };
 
-console.log(getFirstNameTwo(fullName));
+console.log(multiplier.multiply());
--- src/playgrounds/es6-arrow-function.js
+++ src/playgrounds/es6-arrow-function.js
@@ -1,12 +1,45 @@
-const fullName = "Matt Barnett"
+// const fullName = "Matt Barnett"
 
-const getFirstName = (name) => {
-    return name.split(' ')[0];
+// const getFirstName = (name) => {
+//     return name.split(' ')[0];
+// }
+
+// console.log(getFirstName(fullName))
+
+
+// const getFirstNameTwo = (name) => name.split(' ')[1];
+
+// console.log(getFirstNameTwo(fullName))
+
+// arguments object - no longer bound with arrow functions
+// const add =  (a,b) => {
+//     console.log(arguments)
+//     return a+b;
+// }
+
+// console.log(add(55,1))
+
+const user = {
+    name: 'Matt',
+    cities: ['Ontario', 'Yorba Linda', 'Alta Loma'],
+    printPlacesLived() {
+       const cityMessages = this.cities.map((city) => {
+            return this.name + ' has lived in ' + city;
+        })
+        // this.cities.forEach((city) => {
+        //     console.log(this.name + ' has lived in ' + city)
+        // })
+    }
 }
 
-console.log(getFirstName(fullName))
+user.printPlacesLived(user.cities)
 
+const multiplier = {
+    numbers: [1,2,3,4],
+    multiplyBy: 2,
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy)
+    }
+}
 
-const getFirstNameTwo = (name) => name.split(' ')[1];
-
-console.log(getFirstNameTwo(fullName))
+console.log(multiplier.multiply())
