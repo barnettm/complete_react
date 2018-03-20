4b4e9aa961c726fc9353bf771fd0fdc9453b0e47
barnettm <barnett717@gmail.com>
2018-03-20 14:38:45 -0700

    visibility toggle component

--- src/playgrounds/build-it-visible.js
+++ src/playgrounds/build-it-visible.js
@@ -1,34 +1,62 @@
-var appRoot = document.querySelector('#app');
-var details = false;
-var secretInfo = '';
-
-
-
-
-
-
-const toggleDetails = () => {
-    if(!details) {
-        secretInfo = 'This is top secret shit right here';
-
-    }else{
-        secretInfo = ''
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this)        
+        this.state = {
+            visibility: false
+        }
     }
-    details = !details
-    render()
+    handleToggleVisibility(prevState) {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details'}</button>
+                {this.state.visibility && (
+                    <div>
+                        <p>Hey this is some secret stuff</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
 
-const render = () => {
-    var template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <div></div>
-            <button onClick={toggleDetails}>{details ? "Hide Details" : "Show Details"}</button>
-            <p>{secretInfo}</p>
-        </div>
-    );
-    ReactDOM.render(template, appRoot)
-}
 
-render()
+// var appRoot = document.querySelector('#app');
+// var details = false;
+// var secretInfo = '';
+
+// const toggleDetails = () => {
+//     if(!details) {
+//         secretInfo = 'This is top secret shit right here';
+
+//     }else{
+//         secretInfo = ''
+//     }
+//     details = !details
+//     render()
+// }
+
+
+// const render = () => {
+//     var template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <div></div>
+//             <button onClick={toggleDetails}>{details ? "Hide Details" : "Show Details"}</button>
+//             <p>{secretInfo}</p>
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot)
+// }
+
+// render()
