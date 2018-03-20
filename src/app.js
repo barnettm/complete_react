60e89ede59a2159c4f2d8cd6635ba916cceb15da
barnettm <barnett717@gmail.com>
2018-03-19 18:40:23 -0700

    basic components built

--- src/app.js
+++ src/app.js
@@ -1,13 +1,46 @@
 class Header extends React.Component {
     render() {
-        return <p>This is from header</p>
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What Should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <p>Options componenet here</p>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <p>AddOption Compoenent Here</p>
+        );
     }
 }
 
 const jsx = (
     <div>
-        <h1>Title</h1>
          <Header />
+         <Action />
+         <Options />
+         <AddOption />
     </div>
 )
 
