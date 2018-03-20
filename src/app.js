e300742fb0e51b192c2ba89a5eaf697ec9f27286
barnettm <barnett717@gmail.com>
2018-03-19 19:18:46 -0700

    event handlers

--- src/app.js
+++ src/app.js
@@ -27,19 +27,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        
+    }
     render() {
         return (
             <div>
-                <button>What Should I do?</button>
+                <button onClick={this.handlePick}>What Should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll() {
+        alert('remove all')
+    }
     render() {
         return (
             <div>
+               <button onClick={this.handleRemoveAll}>Remove All</button>
                {
                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
                }
