7b6c70e1b5d0cfb7d6eb1f43b348edf6904f5be2
barnettm <barnett717@gmail.com>
2018-03-21 11:22:32 -0700

    lifecycle methods

--- src/app.js
+++ src/app.js
@@ -10,6 +10,17 @@ class IndecisionApp extends React.Component {
         }
     }
 
+    componentDidMount() {
+        console.log('fetching data')
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('saving data')
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
+
     handleDeleteOptions() {
         this.setState(() => ({options: []}))
     }
