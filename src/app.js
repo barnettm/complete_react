0fbc53040afdb8fcc9a75e76761a0091cf8b6f83
barnettm <barnett717@gmail.com>
2018-03-21 10:58:02 -0700

    implicit setState returns

--- src/app.js
+++ src/app.js
@@ -10,11 +10,7 @@ class IndecisionApp extends React.Component {
     }
 
     handleDeleteOptions() {
-        this.setState(() => {
-            return {
-                options: []
-            }
-        })
+        this.setState(() => ({options: []}))
     }
 
     handlePick() {
@@ -30,11 +26,8 @@ class IndecisionApp extends React.Component {
             return 'This option already exists'
         }
         
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat(option)
-            }
-        })
+        this.setState((prevState) => ({options: prevState.options.concat(option)}))
+        
     }
     
     render() {
@@ -123,11 +116,7 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option)
 
-        this.setState(() => {
-            return {
-                error: error
-            }
-        })
+        this.setState(() => ({error}))
     }
     render() {
         return (
