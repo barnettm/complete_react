adcc3ff6f5e9132599565d05c8eb21bc683bd817
barnettm <barnett717@gmail.com>
2018-03-21 12:12:28 -0700

    local storage

--- src/app.js
+++ src/app.js
@@ -11,11 +11,23 @@ class IndecisionApp extends React.Component {
     }
 
     componentDidMount() {
-        console.log('fetching data')
+        try {
+            const json = localStorage.getItem('options')
+            const optionsArray = JSON.parse(json)
+            if(optionsArray) {
+                this.setState(() => ({options: optionsArray}))
+            }
+        } catch (e) {
+            //Do nothing at all
+        }
+
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log('saving data')
+        if(prevState.options.length !== this.state.options.length){
+            const json = JSON.stringify(this.state.options)
+            localStorage.setItem('options', json);
+        }
     }
     componentWillUnmount() {
         console.log('componentWillUnmount')
@@ -107,6 +119,7 @@ const Options = (props) => {
     return (
         <div>
         <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {props.options.length === 0 && <p>Please add an option to get started</p>}
         {
             props.options.map((option) => (
                 <Option 
@@ -149,6 +162,10 @@ class AddOption extends React.Component {
         const error = this.props.handleAddOption(option)
 
         this.setState(() => ({error}))
+
+        if(!error) {
+            e.target.elements.option.value = ''
+        }
     }
     render() {
         return (
