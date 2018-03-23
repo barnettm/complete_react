fc1330331aa73dada4ed4e87ee9eb93449d771b4
barnettm <barnett717@gmail.com>
2018-03-23 14:05:15 -0700

    install react, reactDOM

--- src/app.js
+++ src/app.js
@@ -1,24 +1,8 @@
-// import {square, add} from './utils';
+import validator from 'validator';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
-// console.log(square(2))
-// console.log(add(200,100))
-
-import {isAdult, canDrink} from './person'
-import isSenior from './utils.js';
-
-
-// console.log(subtract(100,30))
-// console.log(`29 is adult? ${isAdult(19)}`)
-// console.log(isAdult(9))
-
-// console.log(canDrink(24))
-// console.log(canDrink(4))
-
-console.log(isSenior(65))
-console.log(isSenior(64))
-
-
-
-
+const template = React.createElement('p', {}, 'Testing ...')
+ReactDOM.render(template, document.getElementById('app'))
 
